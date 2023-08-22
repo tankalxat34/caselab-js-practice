bf4b2f70eee671c103b3b5653d0d4af81925b10e
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-08-22 23:26:44 +0300

    Fixed errors, working on even/odd selection

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
 
 
-var tasks = document.querySelector("#task-field");
 var userinput = document.querySelector("#user-input");
 var p_nothing = document.querySelector("#task-field-nothing");
+var p_completed_nothing = document.querySelector("#task-field-completed_nothing");
 
 
 /**
@@ -10,6 +10,7 @@ var p_nothing = document.querySelector("#task-field-nothing");
  */
 var TaskField = {
     DOM: document.querySelector("#task-field"),
+    DOM_COMPLETED: document.querySelector("#task-field-completed"),
 
     getChilds: function () {
         return document.querySelectorAll("div.taskelem");
@@ -17,6 +18,10 @@ var TaskField = {
 
     count: function () {
         return this.DOM.childElementCount - 1;
+    },
+
+    countCompleted: function () {
+        return this.DOM_COMPLETED.childElementCount - 1;
     },
 
     updateView: function (scroll_status = true) {
@@ -27,7 +32,14 @@ var TaskField = {
             p_nothing.style.display = "inherit";
             document.title = `You are free! - ToDo Manager`;
         }
+
+        if (this.countCompleted()) {
+            p_completed_nothing.style.display = "none";
+        } else {
+            p_completed_nothing.style.display = "inherit";
+        }
         document.querySelector("#h2-actual_tasks").innerText = `Actual tasks (${this.count()})`;
+        document.querySelector("#h2-completed_tasks").innerText = `Completed tasks (${this.countCompleted()})`;
         if (scroll_status) window.scrollTo(0, document.body.scrollHeight);
     },
 
@@ -36,31 +48,35 @@ var TaskField = {
      * @param {Number} task_id task's id
      */
     rm: function (task_id) {
-        let task = document.querySelector(`div.neu.taskelem[data-task_id="${task_id}"]`);
-        task.remove();
-        localStorage.removeItem(task_id)
+        let task = document.querySelector(`div.taskelem[data-task_id="${task_id}"]`);
 
-        this.updateView(false);
+        if (task) {
+            task.remove();
+            localStorage.removeItem(task_id);
+    
+            this.updateView(false);
+        }
     },
 
     /**
-     * Заполняет список задач из `localStorage`
+     * Fills the list of tasks from `localStorage`
      */
     fill: function () {
         if (Object.keys(localStorage).length) {
 
             for (const id of Object.keys(localStorage).sort()) {
-                this.add(localStorage.getItem(id), id);
+                this.draw(localStorage.getItem(id), id, id > 0 ? this.DOM : this.DOM_COMPLETED);
             }
         }
     },
 
     /**
-     * Отрисовывает задачу в перечне задач
-     * @param {String} task_text Текст задачи
-     * @param {Number} task_id Идентификатор задачи (обычно это время). Необязательный аргумент
+     * Draws the task in the task list
+     * @param {String} task_text Task text
+     * @param {Number} task_id Task identifier (usually time). Optional argument
+     * @param {Element} DOM_field Field where needs to place the task
      */
-    draw: function (task_text, task_id) {
+    draw: function (task_text, task_id, DOM_field) {
         let TASK_ID = task_id || new Date().getTime();
 
         let div_keyboard = document.createElement("div");
@@ -69,11 +85,14 @@ var TaskField = {
         div_keyboard.style.position = "inherit";
         div_keyboard.classList = "task-keyboard"
 
-        let kb_complete = document.createElement("button");
-        kb_complete.classList = "neu m0 keyboard_complete";
-        kb_complete.innerText = "✔ Complete";
-        kb_complete.style.margin = "20px 5px"
-        div_keyboard.appendChild(kb_complete);
+        if (DOM_field === TaskField.DOM) {
+            let kb_complete = document.createElement("button");
+            kb_complete.classList = "neu m0 keyboard_complete";
+            kb_complete.innerText = "✔ Complete";
+            kb_complete.style.margin = "20px 5px";
+            kb_complete.addEventListener("click", () => this.complete(TASK_ID));
+            div_keyboard.appendChild(kb_complete);
+        }
 
         let kb_delete = document.createElement("button");
         kb_delete.classList = "neu m0 keyboard_delete";
@@ -82,24 +101,6 @@ var TaskField = {
         kb_delete.addEventListener("click", () => this.rm(TASK_ID));
         div_keyboard.appendChild(kb_delete);
 
-        // let kb_moveup = document.createElement("button");
-        // kb_moveup.classList = "neu m0 keyboard_moveup i";
-        // kb_moveup.innerText = "↑";
-        // kb_moveup.style.margin = "20px 5px";
-        // div_keyboard.appendChild(kb_moveup);
-
-        // let kb_movedown = document.createElement("button");
-        // kb_movedown.classList = "neu m0 keyboard_movedown i";
-        // kb_movedown.innerText = "↓";
-        // kb_movedown.style.margin = "20px 5px";
-        // div_keyboard.appendChild(kb_movedown);
-
-        // kb_complete.hidden = true;
-        // kb_delete.hidden = true;
-        // kb_moveup.hidden = true;
-        // kb_movedown.hidden = true;
-
-
         let div_text = document.createElement("div")
         div_text.classList = "task-text";
         div_text.innerText = task_text;
@@ -107,42 +108,40 @@ var TaskField = {
         let div = document.createElement("div");
         div.setAttribute("data-task_id", TASK_ID);
         div.classList = "neu taskelem";
-        div.innerHTML = `<div style="color: grey;">⏱ ${new Date(new Number(TASK_ID)).toLocaleDateString()} - ${new Date(new Number(TASK_ID)).toLocaleTimeString()}</div>`
+        div.innerHTML = `<p class="task-created_at" style="color: grey;">⏱ ${new Date(new Number(Math.abs(TASK_ID))).toLocaleDateString()} - ${new Date(new Number(Math.abs(TASK_ID))).toLocaleTimeString()}</p>`
+
+        if (DOM_field === TaskField.DOM_COMPLETED) {
+            div.classList.add("line-through");
+        }
 
         div.appendChild(div_text);
         div.appendChild(div_keyboard);
 
         div.addEventListener("click", () => {
-            // kb_complete.hidden = false;
-            // kb_delete.hidden = false;
-            // kb_moveup.hidden = false;
-            // kb_movedown.hidden = false;
-
             div.classList.add("show");
             div_text.classList.add("show");
             div_keyboard.classList.add("show");
         });
         div.addEventListener("mouseleave", () => {
-
-            // kb_complete.hidden = true;
-            // kb_delete.hidden = true;
-            // kb_moveup.hidden = true;
-            // kb_movedown.hidden = true;
-
-
             div.classList.remove("show");
             div_text.classList.remove("show");
             div_keyboard.classList.remove("show");
         });
 
-        this.DOM.appendChild(div);
+        DOM_field.appendChild(div);
         this.updateView();
 
         return TASK_ID;
     },
 
-    add: function (task_text, task_id) {
-        localStorage.setItem(this.draw(task_text, task_id), task_text);
+    /**
+     * Save task to local storage
+     * @param {Number} task_id The task's id
+     * @param {String} task_text The task's text
+     * @param {Element} DOM_field Field where needs to place the task
+     */
+    saveToLocalStorage: function (task_id, task_text, DOM_field) {
+        localStorage.setItem(this.draw(task_text, task_id, DOM_field), task_text);
     },
 
     /**
@@ -159,21 +158,58 @@ var TaskField = {
         TaskField.rm(new Number(Object.keys(localStorage).sort().pop()));
     },
 
+    /**
+     * Mark the task as completed
+     * @param {Number} task_id The task's id
+     */
+    complete: function (task_id) {
+        let task_text = document.querySelector(`div.taskelem[data-task_id="${task_id}"] > div.task-text`);
+        let task_kb_complete = document.querySelector(`div.taskelem[data-task_id="${task_id}"] > div.task-keyboard > button.keyboard_complete`);
+        task_kb_complete.hidden = true;
+
+        let completed_task_id = -task_id;
+        this.rm(task_id);
+        this.saveToLocalStorage(completed_task_id, task_text.innerText, this.DOM_COMPLETED);
+    },
+
     addByUserInput: function () {
         if (userinput.value && userinput.value.length <= 200) {
-            TaskField.add(userinput.value, new Date().getTime())
+            TaskField.saveToLocalStorage(new Date().getTime(), userinput.value, TaskField.DOM);
             userinput.value = new String();
         }
+    },
+
+    markOnlyEven: function () {
+        let i = 0;
+        TaskField.getChilds().forEach(el => {
+            el.classList.remove("selection");
+            if (i % 2 === 0) el.classList.add("selection");
+            i++;
+        });
+    },
+
+    markOnlyOdd: function () {
+        let i = 0;
+        TaskField.getChilds().forEach(el => {
+            el.classList.remove("selection");
+            if (i % 2 != 0) el.classList.add("selection");
+            i++;
+        });
     }
 }
+
 
 
 TaskField.fill();
 TaskField.updateView();
 
-document.querySelector("#button-addtolist").addEventListener("click", TaskField.addByUserInput);
-document.querySelector("#kbmain-shift").addEventListener("click", TaskField.shift);
-document.querySelector("#kbmain-pop").addEventListener("click", TaskField.pop);
+window.onload = () => {
+    document.querySelector("#button-addtolist").addEventListener("click", TaskField.addByUserInput);
+    document.querySelector("#kbmain-shift").addEventListener("click", TaskField.shift);
+    document.querySelector("#kbmain-pop").addEventListener("click", TaskField.pop);
+    document.querySelector("#kbmain-show-even").addEventListener("click", TaskField.markOnlyEven);
+    document.querySelector("#kbmain-show-odd").addEventListener("click", TaskField.markOnlyOdd);
+}
 
 userinput.addEventListener("keydown", (e) => {
     if (e.keyCode === 13) TaskField.addByUserInput();
